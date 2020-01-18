b554fed42063319395255977c41553024f31093e
Philip Bauer <bauer@starzel.de>
2020-01-18 15:46:09 +0100

    fix template

--- src/plonede.policy/plonede/policy/browser/frontpage.py
+++ src/plonede.policy/plonede/policy/browser/frontpage.py
@@ -15,10 +15,36 @@ class FrontPageView(BrowserView):
         return self.index()
 
     def news(self):
+        results = []
         brains = api.content.find(
             portal_type='News Item',
             sort_on='effective',
             sort_order='reverse',
             review_state='published',
-            )
-        return brains
+            sort_limit=4,
+            )[:4]
+        for brain in brains:
+            obj = brain.getObject()
+            # if getattr(obj.aq_base, 'image'):
+
+            scales = api.content.get_view(
+                name='images',
+                context=obj,
+                request=self.request)
+            scale = scales.scale(
+                'image',
+                width=400,
+                height=200,
+                direction='down')
+            tag = scale.tag() if scale else None
+
+            # 64, 64,
+            # src = obj.
+            results.append({
+                'title': brain.Title,
+                'description': brain.Description,
+                'url': brain.getURL(),
+                # 'src': brain.getURL(),
+                'tag': tag,
+                })
+        return results
